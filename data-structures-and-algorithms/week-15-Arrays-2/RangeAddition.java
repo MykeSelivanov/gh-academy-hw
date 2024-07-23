33c82e8772211139b1cdd02520e7e3851f88cfcc
MykeSelivanov <michael777selivanov@gmail.com>
2024-07-22 23:51:38 -0500

    week 15

---
+++ data-structures-and-algorithms/week-15-Arrays-2/RangeAddition.java
@@ -0,0 +1,12 @@
+public class Solution {
+    // Method to compute the modified array after a sequence of updates
+    public int[] getModifiedArray(int length, int[][] updates) {
+        // Create an array 'differehce' initialized to zero, with the given length
+        int[] difference = new int[length];
+
+        // Apply each update in the updates array
+        for (int[] update: updates) {
+            int startIndex = update[0];
+        }
+    }
+}
