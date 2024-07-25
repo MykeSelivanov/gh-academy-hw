77e8ab16d022aad8507640bd1d010a349d1ec13e
MykeSelivanov <michael777selivanov@gmail.com>
2024-07-24 22:48:23 -0500

    week 15

--- data-structures-and-algorithms/week-15-Arrays-2/REPORT.md
+++ data-structures-and-algorithms/week-15-Arrays-2/REPORT.md
@@ -11,7 +11,7 @@ Status colors list:
 - 🟢 [LC 141. Linked List Cycle](https://leetcode.com/problems/linked-list-cycle/) - [Solution](LinkedListCycle.java)
 - 🟢 [LC 142. Linked List Cycle II](https://leetcode.com/problems/linked-list-cycle-ii/) - [Solution](LinkedListCycleII.java)
 - 🟢 [239. Sliding Window Maximum](https://leetcode.com/problems/sliding-window-maximum/) - [Solution]()
-- 🟡 [370. Range Addition](https://leetcode.com/problems/range-addition/) - [Solution]()
+- 🟢 [370. Range Addition](https://leetcode.com/problems/range-addition/) - [Solution](RangeAddition.java)
 
 ## Recommended Homework
 
--- data-structures-and-algorithms/week-15-Arrays-2/RangeAddition.java
+++ data-structures-and-algorithms/week-15-Arrays-2/RangeAddition.java
@@ -7,6 +7,26 @@ public class Solution {
         // Apply each update in the updates array
         for (int[] update: updates) {
             int startIndex = update[0];
+            int endIndex = update[1];
+            int increment = update[2];
+
+            // Apply increment to the start index
+            difference[startIndex] += increment;
+
+            // If the index is not the last element of the array
+            // apply the negation of increment to the elemenent after the endIndex
+            if (endIndex + 1 < length) {
+                difference[endIndex + 1] -= increment;
+            }
         }
+
+        // Convert the 'difference' array into the actual array 'result'
+        // where each element is the cumulative sum from start to that index
+        for (int i = 1; i < length; i++) {
+            difference[i] += difference[i-1];
+        }
+
+        // Return the resulted modified array
+        return difference;
     }
 }
